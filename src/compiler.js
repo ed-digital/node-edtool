f334def66b01f8f3ce9f3b46da31cc86790c3bd4
Kieran Browne <kierantbrowne@gmail.com>
2016-11-24 10:39:28 +1100

    added sass support; we may need to add a flag for choosing between sass and less

--- src/compiler.js
+++ src/compiler.js
@@ -18,6 +18,8 @@ const buffer = require('vinyl-buffer');
 const gulp = require('gulp');
 const gulpWatch = require('gulp-watch');
 const less = require('gulp-less');
+// const sass = require('node-sass');
+const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
 // module.exports = function(workingDir, options) {
@@ -84,6 +86,7 @@ class Compiler extends EventEmitter {
 	
 	compile(watch) {
 		this.compileLESS(watch);
+		this.compileSASS(watch);
 		this.compileJS(watch);
 		// this.compileHTML(watch);
 	}
@@ -129,6 +132,51 @@ class Compiler extends EventEmitter {
 		if(watch) {
 			gulpWatch(this.assetPath+'/less/**/*', () => {
 				console.log(chalk.magenta("------- Detected changes in LESS -------"));
+				compileAll();
+			});
+		}
+		
+		compileAll();
+		
+	}
+
+	compileSASS(watch) {
+		
+		console.log(chalk.yellow(">> Compiling SASS"));
+		
+		let files = ['screen.scss'];
+		
+		let compile = (file) => {
+			
+			gulp.src(this.assetPath+'/sass/'+file)
+				.pipe(sass().on('error', sass.logError))
+				.pipe(autoprefixer())
+				.on('error', (err) => {
+					
+					console.log(chalk.black(chalk.bgRed(">> SASS Compiler Error")));
+					console.log(err.message);
+					
+					this.addError('sass', `Failed to compile ${file}`, err.message);
+					
+				})
+				.pipe(gulp.dest('./assets-built/css'))
+				.on('end', () => {
+					console.log(chalk.cyan(">> Finished compiling "+file));
+					this.changed();
+				});
+			
+		};
+		
+		let compileAll = () => {
+			this.clearErrors('sass');
+			for(let file of files) {
+				compile(file);
+			}
+		};
+		
+		if(watch) {
+			gulpWatch(this.assetPath+'/sass/**/*', () => {
+				console.log(chalk.magenta("------- Detected changes in SASS -------"));
 				compileAll();
 			});
 		}
